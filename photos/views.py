bd3353e02b87ce591cfb12d7078a83489ae6e62f
WambiruL <wambirulorraine@gmail.com>
2021-07-02 16:27:58 +0300

    Style homepage

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    '''
+    Function that displays the homepage of the application
+    '''
+    return render(request,'index.html')

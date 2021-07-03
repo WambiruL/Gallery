bc01fd8ae5815e1fef79aec476717a667ba2b0cd
WambiruL <wambirulorraine@gmail.com>
2021-07-03 19:12:10 +0300

    Create the location and category models

--- photos/models.py
+++ photos/models.py
@@ -38,3 +38,38 @@ class Image(models.Model):
     
     def __str__(self):
         return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
+
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=60)
+
+    @classmethod
+    def get_locations(cls):
+        locations = Location.objects.all()
+        return locations
+
+    @classmethod
+    def update_location(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+    
+    def __str__(self):
+        return self.name
--- photos/views.py
+++ photos/views.py
@@ -2,9 +2,9 @@ from .models import Image
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
+def gallery(request):
     '''
     Function that displays the photos
     '''
     gallery=Image.objects.all()
-    return render(request,'index.html', {'gallery':gallery})
+    return render(request,'gallery.html', {'gallery':gallery})

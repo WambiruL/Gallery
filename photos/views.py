f199b484735c55a1e632633d13487ecde78016fe
WambiruL <wambirulorraine@gmail.com>
2021-07-06 01:19:10 +0300

    Added the search functionality, deleted the navbar.html and took its contents to index.html

--- photos/views.py
+++ photos/views.py
@@ -17,3 +17,12 @@ def image(request,article_id):
     except Image.DoesNotExist:
         raise Http404()
     return render(request,"single_image.html", {"image":image})
+
+def search_results(request):
+
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_image(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"images": searched_images})

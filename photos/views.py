6407f09b06a02a7917df94aa1d26dedf125799fa
WambiruL <wambirulorraine@gmail.com>
2021-07-06 02:04:37 +0300

    Add the categories to the navbar and urls

--- photos/views.py
+++ photos/views.py
@@ -1,4 +1,4 @@
-from .models import Image, Location
+from .models import Image, Location, Category
 from django.shortcuts import render
 from django.http import Http404
 
@@ -25,4 +25,25 @@ def search_results(request):
         searched_images = Image.search_image(search_term)
         message = f"{search_term}"
 
-        return render(request, 'search.html',{"message":message,"images": searched_images})
+    return render(request, 'search.html',{"message":message,"images": searched_images})
+
+def nature(request):
+    nature_category = Category.objects.get(pk=1)
+    nature = Image.objects.filter(category=nature_category)
+    return render(request,'nature.html', {'nature':nature})
+
+def fashion(request):
+    fashion_category = Category.objects.get(pk=2)
+    fashion = Image.objects.filter(category=fashion_category)
+    return render(request,'fashion.html', {'fashion':fashion})
+
+def food(request):
+    food_category = Category.objects.get(pk=2)
+    food = Image.objects.filter(category=food_category)
+    return render(request,'food.html', {'food':food})
+
+def animals(request):
+    animals_category = Category.objects.get(pk=2)
+    animals = Image.objects.filter(category=animals_category)
+    return render(request,'animals.html', {'animals':animals})
+

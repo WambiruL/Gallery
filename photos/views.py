36a999e9fc48c2301b9a1d8f3801294629bf3bb2
WambiruL <wambirulorraine@gmail.com>
2021-07-06 01:03:22 +0300

    Add the single-image template

--- photos/views.py
+++ photos/views.py
@@ -1,5 +1,6 @@
 from .models import Image, Location
 from django.shortcuts import render
+from django.http import Http404
 
 # Create your views here.
 def index(request):
@@ -10,3 +11,9 @@ def index(request):
     location=Location.get_locations()
     return render(request,'index.html', {'images':images,'location':location})
 
+def image(request,article_id):
+    try:
+        image = Image.objects.get(id = article_id)
+    except Image.DoesNotExist:
+        raise Http404()
+    return render(request,"single_image.html", {"image":image})

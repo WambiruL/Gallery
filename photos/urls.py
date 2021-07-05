48d4340370356c0092acf50617b72bb419858914
WambiruL <wambirulorraine@gmail.com>
2021-07-06 01:35:16 +0300

    Link the search functionality to the urls

--- photos/urls.py
+++ photos/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns=[
     url(r'^$',views.index,name = 'index'),
+    url(r'^$',views.image, name='image'),
+    url(r'^search/',views.search_results,name='results')
 ]
+
+

6d12f137c0aa63f6fbaefd8245fb114e79e2ffa9
WambiruL <wambirulorraine@gmail.com>
2021-07-07 02:51:18 +0300

    Port

--- myGallery/settings.py
+++ myGallery/settings.py
@@ -89,7 +89,7 @@ if config('MODE')=="dev":
             'USER': config('DB_USER'),
             'PASSWORD':config('DB_PASSWORD'),
             'HOST':config('DB_HOST'),
-            'PORT':'',
+            'PORT':'5432',
     }
 }
 else:
